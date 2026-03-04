aa2f7b7204f6d2ffe9f1594cd5dec1e62d00c9bd
Tri <you@example.com>
2026-03-04 07:38:51 +0700

    Translate command arguments (e.g. /whisper Trivnpk chào -> /whisper Trivnpk hello)

--- src/client/java/io/gitlab/jfronny/googlechat/client/mixin/ChatScreenMixin.java
+++ src/client/java/io/gitlab/jfronny/googlechat/client/mixin/ChatScreenMixin.java
@@ -1,6 +1,7 @@
 package io.gitlab.jfronny.googlechat.client.mixin;
 
 import io.gitlab.jfronny.googlechat.GoogleChat;
+import io.gitlab.jfronny.googlechat.GoogleChatConfig;
 import io.gitlab.jfronny.googlechat.TranslationDirection;
 import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +12,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class ChatScreenMixin {
     @ModifyVariable(method = "handleChatInput(Ljava/lang/String;Z)V", at = @At(value = "HEAD"), argsOnly = true, ordinal = 0)
     String googlechat$translateChatText(String chatText) {
-        if (chatText.startsWith("/")) return chatText; // Bypass for client-side commands (Carpet, ...)
+        if (!GoogleChatConfig.General.enabled) return chatText;
+        
+        if (chatText.startsWith("/")) {
+            int spaceIndex = chatText.indexOf(' ');
+            if (spaceIndex > 0) {
+                String command = chatText.substring(0, spaceIndex);
+                String args = chatText.substring(spaceIndex + 1);
+                String translatedArgs = GoogleChat.translateIfNeeded(args, TranslationDirection.C2S, true);
+                return command + " " + translatedArgs;
+            }
+            return chatText;
+        }
+        
         return GoogleChat.translateIfNeeded(chatText, TranslationDirection.C2S, true);
     }
 }
